9c94c6c95e8dc0cb07bd472020c2dfa58fe58f2c
Ilham M <cuanizenkahiji@gmail.com>
2026-04-03 11:11:53 +0700

    Base Models & Controllers - (Update) Kita Mengubah Front Controller ini agar bekerja dengan instansiasi Object

--- index.php
+++ index.php
@@ -1,30 +1,19 @@
 <?php
-// index.php (FRONT CONTROLLER)
 session_start();
 require_once 'controllers/AuthController.php';
 
-
-// Tangkap perintah dari URL (misal: index.php?action=login)
-// Jika tidak ada parameter action, set default ke 'login'
+// Tangkap perintah dari URL
 $action = isset($_GET['action']) ? $_GET['action'] : 'login';
 
+// Buat object dari Controller
+$controller = new AuthController();
 
-// Arahkan ke fungsi Controller yang sesuai
-switch ($action) {
-    case 'login':
-        handleLogin();
-        break;
-    case 'register':
-        handleRegister();
-        break;
-    case 'dashboard':
-        showDashboard();
-        break;
-    case 'logout':
-        handleLogout();
-        break;
-    default:
-        handleLogin();
-        break;
+// Cek apakah method (fungsi) ada di dalam controller tersebut
+if (method_exists($controller, $action)) {
+    // Panggil method secara dinamis
+    $controller->$action();
+} else {
+    // Jika user mengetik sembarangan di URL, kembalikan ke login
+    $controller->login();
 }
 ?>
