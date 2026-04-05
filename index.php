c87ecf316f6a8cad192b0af98bf1d0a0e0a27961
Ilham M <cuanizenkahiji@gmail.com>
2026-04-06 01:15:42 +0700

    Middleware Log - (Update) Kita panggil LogMiddleware ini tepat setelah session_start(), agar dia dieksekusi pertama kali sebelum middleware rute (satpam halaman) bekerja.

--- index.php
+++ index.php
@@ -1,15 +1,22 @@
 <?php
+// index.php
 session_start();
 
 // Panggil semua file yang dibutuhkan
+require_once 'middlewares/LogMiddleware.php';
 require_once 'middlewares/AuthMiddleware.php';
 require_once 'middlewares/GuestMiddleware.php';
 require_once 'controllers/AuthController.php';
 
+// --- EKSEKUSI GLOBAL MIDDLEWARE ---
+// Mencatat semua aktivitas tanpa terkecuali!
+LogMiddleware::handle();
+// ----------------------------------
+
 // Tangkap perintah dari URL
 $action = isset($_GET['action']) ? $_GET['action'] : 'login';
 
-// TABEL ROUTING: Mendefinisikan rute, controller, dan satpam (middleware) penjaganya
+// TABEL ROUTING
 $routes = [
     'login'     => ['method' => 'login',     'middleware' => 'GuestMiddleware'],
     'register'  => ['method' => 'register',  'middleware' => 'GuestMiddleware'],
@@ -17,27 +24,23 @@ $routes = [
     'logout'    => ['method' => 'logout',    'middleware' => 'AuthMiddleware']
 ];
 
-// Instansiasi Controller
 $controller = new AuthController();
 
-// Cek apakah action yang diminta user ada di tabel routing kita
 if (array_key_exists($action, $routes)) {
     
     $route = $routes[$action];
     $middlewareClass = $route['middleware'];
     $methodName = $route['method'];
 
-    // 1. EKSEKUSI MIDDLEWARE DULU (Cek Tiket / Satpam)
+    // EKSEKUSI ROUTE MIDDLEWARE (Satpam Khusus Halaman)
     if ($middlewareClass !== null) {
-        // Contoh: AuthMiddleware::handle();
         $middlewareClass::handle(); 
     }
 
-    // 2. JIKA LOLOS MIDDLEWARE, EKSEKUSI CONTROLLER
+    // EKSEKUSI CONTROLLER
     $controller->$methodName();
 
 } else {
-    // Jika rute tidak ditemukan (404), kembalikan ke login
     header("Location: index.php?action=login");
     exit;
 }
